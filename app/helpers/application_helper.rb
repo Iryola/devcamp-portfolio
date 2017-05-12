07e4b1654eb5b5704967b19ef8a1cac349aeda90
Steve <adamfam@hotmail.com>
2017-05-11 22:06:21 -0600

    Implemented collection based partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,8 +12,7 @@ module ApplicationHelper
  def source_helper(layout_name)
   if session[:source]
     greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source-greeting")
+    content_tag(:p, greeting, class: "source-greeting")
     end
   end
-
 end
