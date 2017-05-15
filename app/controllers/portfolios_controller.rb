54c89d452e9cdca8dfb268ff6943d0d00f0446f3
Steve <adamfam@hotmail.com>
2017-05-15 16:03:51 -0600

    Refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,57 +1,54 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   
   def index
     @portfolio_items = Portfolio.all
-      end
+  end
 
-      def angular
-          @angular_portfolio_items = Portfolio.angular
-      end
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
 
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
 
-    def create
+  def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+    if @portfolio_item.save
+      format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
         
-      else
-        format.html { render :new }
+  else
+    format.html { render :new }
       
       end
     end
   end
 
-    def edit
-      @portfolio_item = Portfolio.find(params[:id])
+  def edit
+  end
 
-    end
-
-      def update
-        @portfolio_item = Portfolio.find(params[:id])
-
-        respond_to do |format|
-        if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-      else
-        format.html { render :edit }
+  def update
+    respond_to do |format|
+    if @portfolio_item.update(portfolio_params)
+    format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+    else
+    format.html { render :edit }
       end
     end
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
+     binding.pry
   end
 
   def destroy
     #This is going to perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
+    
     #This is going to Destroy
     @portfolio_item.destroy
 
@@ -69,6 +66,10 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       technologies_attributes: [:name]
                                       )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   end
