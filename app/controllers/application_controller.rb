093285c05e5f50da0bb02fd0716028e0c7eba1aa
Steve <adamfam@hotmail.com>
2017-05-16 20:50:34 -0600

    Integrated new gem into rails app for new copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-
 end
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
     content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    StevenViewTool::Renderer.copyright 'Steven Adams', 'All right reserved'
+  end
 end
