aa79810b49c87c57984ae14290e01935fed4543a
Steve <adamfam@hotmail.com>
2017-08-11 12:29:09 -0600

    refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,4 +60,16 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Steven Adams Portfolio", sticky: false)
+  end
 end
