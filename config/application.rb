baf79d53c1b72e1e8113a9c6fc7ed9d6a704ff45
Steve <adamfam@hotmail.com>
2018-03-04 17:31:00 -0700

    implemented initial connection to twitter API

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ Bundler.require(*Rails.groups)
 
 module DevcampPortfolio
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
